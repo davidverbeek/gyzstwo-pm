0cb464555db81dd76e53243cdb14d88739d60e99
David Verbeek <dverbeek.2019@gmail.com>
2023-04-24 19:42:49 +0530

    New changes

--- src/app/modules/admin/layout/left/left/left.component.ts
+++ src/app/modules/admin/layout/left/left/left.component.ts
@@ -1,16 +1,94 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+declare var simTree: any;
+import { AppConstants } from "src/app/app-constants";
+import { HttpClient } from '@angular/common/http';
+import { PmCategoryService } from 'src/app/pm.category.service';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-left',
   templateUrl: './left.component.html',
-  styleUrls: ['./left.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./left.component.css']
 })
 export class LeftComponent implements OnInit {
 
-  constructor() { }
+  list: string;
+  constructor(private http: HttpClient, private categoryService: PmCategoryService) { }
 
-  ngOnInit(): void {
+  @ViewChild('allSelectedCats') allSelectedCats: ElementRef;
+
+  /*list = [{
+    "id": '1',
+    "pid": '',
+    "name": "JavaScript",
+  },
+  {
+    "id": '11',
+    "pid": '1', // parent ID
+    "name": "Angular"
+  },
+  {
+    "id": '12',
+    "pid": '1',
+    "name": "React"
+  }, {
+    "id": '13',
+    "pid": '1',
+    "name": "Vuejs"
+  }, {
+    "id": '14',
+    "pid": '1',
+    "name": "jQueryScript.Net"
+  },
+  {
+    "id": '2',
+    "pid": '',
+    "name": "HTML5"
+  },
+  {
+    "id": '3',
+    "pid": '',
+    "name": "CSS3",
+    "disabled": true
+  }]; */
+
+
+
+  ngOnInit() {
+    console.log("left");
+    this.http.get<any>(AppConstants.webservicebaseUrl + "/all-categories").subscribe(data => {
+      this.list = data.categories;
+      //console.log(data.categories);
+      
+      var tree = simTree({
+        el: '#tree',
+        data: this.list,
+        check: true,
+        linkParent: true,
+        //check: true,
+        onClick: function (item) {
+          //console.log(item);
+
+        },
+        onChange: function (item) {
+          var selectedCategories = new Array();
+          $.each( item, function( key, value ) {
+              selectedCategories.push(value["id"]);
+          });
+          $("#selected_cats").val(selectedCategories);
+          $("#btnloadcats").click();
+        }
+      });
+    })
   }
 
+  btncats() {
+    this.categoryService.categorySelected.next(this.allSelectedCats.nativeElement.value);
+  } 
+
 }
+
+
+
+
+
--- src/app/modules/admin/pages/setprices/setprices/setprices.component.ts
+++ src/app/modules/admin/pages/setprices/setprices/setprices.component.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
-import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
-import { Observable } from 'rxjs';
+import { ColDef, GridReadyEvent,IServerSideDatasource,ServerSideStoreType,RowClassParams, CellValueChangedEvent, CellEditingStoppedEvent, DragStoppedEvent, CellKeyPressEvent} from 'ag-grid-community';
+import { Observable, UnsubscriptionError } from 'rxjs';
 import 'ag-grid-enterprise';
+import { AppConstants } from "src/app/app-constants";
+import { PmCategoryService } from 'src/app/pm.category.service';
 
 @Component({
   selector: 'app-setprices',
@@ -12,21 +14,72 @@ import 'ag-grid-enterprise';
 })
 export class SetpricesComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  api: any;
+  gridParams: any;
+  columnApi: any;
+  cats: String = "";
+  categoryChanged: String = "0";
+  updatedProducts: any = [];
 
-  ngOnInit() { }
+
+  public rowModelType: 'clientSide' | 'infinite' | 'viewport' | 'serverSide' = 'serverSide';
+  public serverSideStoreType: ServerSideStoreType = 'partial';
+  public fillHandleDirection: 'x' | 'y' | 'xy' = 'x';
+
+  //public rowModelType: 'serverSide';
+
+  public paginationPageSize = 200;
+  public cacheBlockSize = 200;
+  rowStyle = { background: '' };
+  getRowStyle = (params: RowClassParams) => this.rowStyle;
+
+  constructor(private http: HttpClient, private categoryService: PmCategoryService) {}
+
+  ngOnInit() {
+    console.log("price");
+    this.categoryService.categorySelected.subscribe((allselectedcats) => {
+      //console.log(allselectedcats);
+      this.cats = allselectedcats;
+      this.loadAGGrid();
+      //console.log(allselectedcats);
+    })
+    //this.categoryChanged = "1";
+    
+  }
 
   // Each Column Definition results in one Column.
   public columnDefs: ColDef[] = [
-    { field: 'make', rowGroup: true },
-    { field: 'model' },
-    { field: 'price' }
+    { field: 'product_id', headerName: 'ID',sortable: true, filter: 'number' },
+    {
+      field: 'supplier_type', headerName: 'Leverancier',sortable: true, filter: 'agSetColumnFilter', filterParams: {
+        values: ['Gyzs', 'JRS', 'Transferro']
+      }
+    },
+    { field: 'name', headerName: 'Naam', sortable: true, filter: 'text' },
+    { field: 'sku', headerName: 'SKU',  sortable: true, filter: 'text' },
+    { field: 'supplier_sku', headerName: 'SKU (Sup)', sortable: true, filter: 'text'},
+    { field: 'eancode', headerName: 'Ean', sortable: true, filter: 'text' },
+    { field: 'merk', headerName: 'Merk', sortable: true, filter: 'text' },
+    { field: 'idealeverpakking', headerName: 'Ideal.verp', sortable: true, filter: 'number' },
+    { field: 'afwijkenidealeverpakking', headerName: 'Afw.Ideal.verp', sortable: true, filter: 'number' },
+    { field: 'buying_price', headerName: 'PM Inkpr', sortable: true, filter: 'number' },
+    { field: 'selling_price', headerName: 'PM Vkpr', sortable: true, filter: 'number', editable: true, cellStyle: { 'background-color': '#ffffcc' } },
+    { field: 'profit_percentage', headerName: 'Marge Inkpr %', sortable: true, filter: 'number', editable: true, singleClickEdit: true, cellStyle: { 'background-color': '#ffffcc' } },
+    { field: 'profit_percentage_selling_price', headerName: 'Marge Verkpr %', sortable: true, filter: 'number', editable: true, singleClickEdit: true, cellStyle: { 'background-color': '#ffffcc' }  },
+    { field: 'discount_on_gross_price', headerName: 'Korting Brupr %', sortable: true, filter: 'number', editable: true, singleClickEdit: true, cellStyle: { 'background-color': '#ffffcc' }  },
+    { field: 'percentage_increase', headerName: 'Stijging %', sortable: true, filter: 'number'  },
+    { field: 'magento_status', headerName: 'Status', sortable: true, filter: 'number'  }
+
   ];
+
 
   // DefaultColDef sets props common to all Columns
   public defaultColDef: ColDef = {
     sortable: true,
     filter: true,
+    flex: 1,
+    minWidth: 100,
+    resizable: true
   };
 
   // Data that gets displayed in the grid
@@ -37,17 +90,79 @@ export class SetpricesComponent implements OnInit {
 
   // Example load data from sever
   onGridReady(params: GridReadyEvent) {
-    this.rowData$ = this.http
-      .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+    this.api = params.api;
+    this.gridParams = params;
+    this.columnApi = params.columnApi;
+    this.loadAGGrid();
+    /* this.rowData$ = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json'); */
+    //this.rowData$ = this.http.get<any[]>('https://hipexstaging2:QJfn2pstmS8pZrY@staging2.gyzs.nl/pdata/pdata.json');
   }
 
-  // Example of consuming Grid Event
-  onCellClicked(e: CellClickedEvent): void {
-    console.log('cellClicked', e);
-  }
 
-  // Example using Grid's API
-  clearSelection(): void {
-    this.agGrid.api.deselectAll();
+  loadAGGrid() {
+    var datasource = createServerSideDatasource(this.gridParams,this.cats);
+    this.api.setServerSideDatasource(datasource);
+    this.columnApi.setColumnVisible('product_id', false);
+    this.columnApi.setColumnVisible('supplier_sku', false);
+    this.columnApi.setColumnVisible('eancode', false);
+    this.columnApi.setColumnVisible('idealeverpakking', false);
+    this.columnApi.setColumnVisible('afwijkenidealeverpakking', false);
+    this.columnApi.setColumnVisible('magento_status', false);
+    this.fillHandleDirection = 'y';
   }
+  onCellValueChanged(event: CellValueChangedEvent) {
+    var productData = {};
+    productData["buying_price"] = event.data.buying_price;
+    productData["selling_price"] = event.data.selling_price;
+    productData["profit_percentage"] = event.data.profit_percentage;
+    productData["profit_percentage_selling_price"] = event.data.profit_percentage_selling_price;
+    productData["discount_on_gross_price"] = event.data.discount_on_gross_price;
+    productData["percentage_increase"] = event.data.percentage_increase;
+    this.updatedProducts[event.data.product_id] = productData; 
+  }
+ 
+  onCellEditingStopped(event: CellEditingStoppedEvent) {
+    console.log(this.updatedProducts);
+    this.updatedProducts = [];
+  }
+  onDragStopped(event: DragStoppedEvent) {
+    console.log(this.updatedProducts);
+    this.updatedProducts = [];
+  }
+  
+  
+
+}
+
+
+
+function createServerSideDatasource(server: any,cats: any): IServerSideDatasource {
+  return {
+    getRows(params) {
+      
+      params.request["cats"] = cats;
+      //console.log(params.request);
+      fetch(AppConstants.webservicebaseUrl + "/pm-products", {
+        method: 'post',
+        body: JSON.stringify(params.request),
+        headers: { "Content-Type": "application/json; charset=utf-8" }
+      })
+        .then(httpResponse => httpResponse.json())
+        .then(response => {
+         
+          //console.log(response);
+          if(response.lastRow == null) {
+            response.lastRow = 0;
+          } 
+          params.successCallback(response.rows, response.lastRow);
+          
+          //params.success({ rowData: response.rows })
+         
+        })
+        .catch(error => {
+          params.failCallback();
+          //params.fail();
+        })
+    }
+  };
 }
