727c3a5239a1f3421cd02c149418aabff877636c
David Verbeek <dverbeek.2019@gmail.com>
2023-09-04 11:54:18 +0530

    Bol Minimum Feature

--- src/app/modules/admin/pages/bolminimum/bolminimum/bolminimum.component.ts
+++ src/app/modules/admin/pages/bolminimum/bolminimum/bolminimum.component.ts
@@ -3,6 +3,8 @@ import { environment } from 'src/environments/environment';
 import { GridReadyEvent, IServerSideDatasource, ServerSideStoreType, RowClassParams } from 'ag-grid-community';
 import { CommonService } from 'src/app/services/common.service';
 import { BolmincalculationComponent } from '../bolmincalculation/bolmincalculation.component';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-bolminimum',
@@ -24,22 +26,42 @@ export class BolminimumComponent implements OnInit {
   historyEan = "";
   historyPid = "";
   subHistoryPid: any;
+  ecDeliveryTimes: any;
+  ecDeliveryTimesBE: any;
+  chkAllCount: string;
+  chkAllProducts: any;
+  isChkAllChecked: number = 0;
+  updatedBolProducts: any = [];
 
   // Data that gets displayed in the grid
   public rowData: any;
 
-  constructor(private CommonService: CommonService) { }
+  constructor(private CommonService: CommonService, private http: HttpClient) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.updatedBolProducts = [];
+
+    this.http.get(environment.webservicebaseUrl + "/get-ec-deliverytimes").subscribe(ecDeliveryTimes => {
+      this.ecDeliveryTimes = ecDeliveryTimes["msg"];
+    }
+    );
+
+    this.http.get(environment.webservicebaseUrl + "/get-ec-deliverytimes-be").subscribe(ecDeliveryTimesBE => {
+      this.ecDeliveryTimesBE = ecDeliveryTimesBE["msg"];
+    }
+    );
+
+  }
 
   // Each Column Definition results in one Column.
   public columnDefs = [
+    { field: 'product_id', headerName: 'ID', sortable: true, filter: 'number', hide: true },
     { field: 'product_sku', headerName: 'SKU', sortable: true, filter: 'text', cellRenderer: BolmincalculationComponent },
     { field: 'supplier_type', headerName: 'Supplier', sortable: true, filter: 'text' },
     { field: 'product_bol_minimum_price', headerName: 'Bol Min. Price', sortable: true, filter: 'number' },
     { field: 'product_bol_selling_price', headerName: 'Bol Selling Price', sortable: true, filter: 'number' },
-    { field: 'ec_deliverytime', headerName: 'EC Delivery Time', sortable: true, filter: 'text' },
-    { field: 'ec_deliverytime_be', headerName: 'EC Delivery Time BE', sortable: true, filter: 'number' },
+    { field: 'ec_deliverytime_text', headerName: 'EC Delivery Time', sortable: true, filter: 'text' },
+    { field: 'ec_deliverytime_be_text', headerName: 'EC Delivery Time BE', sortable: true, filter: 'text' },
     { field: 'updated_date_time', headerName: 'Updated Date', sortable: true }
   ];
 
@@ -63,6 +85,87 @@ export class BolminimumComponent implements OnInit {
     var datasource = createServerSideDatasource(this.gridParams);
     this.api.setServerSideDatasource(datasource);
   }
+
+  updateEcDeliveryTime(event) {
+    console.log(event.target.id);
+    var idsToUpdate = this.api.getSelectedNodes().map(function (node) {
+      return node.data.product_id;
+    });
+
+    var type = event.target.id;
+    var dt = event.target.value;
+
+    if (idsToUpdate.length == 0) {
+      alert("Please select record first!!");
+    } else {
+      if (this.isChkAllChecked == 0) {
+        this.api.forEachNode((rowNode) => {
+          if (idsToUpdate.indexOf(rowNode.data.product_id) >= 0) {
+            var updated = JSON.parse(JSON.stringify(rowNode.data));
+            var prepareProductData = {};
+            prepareProductData["product_id"] = updated.product_id;
+            prepareProductData["ec_deliverytime"] = dt;
+            this.updatedBolProducts[updated.product_id] = prepareProductData;
+          }
+        });
+      } else if (this.isChkAllChecked == 1) {
+        if ((this.chkAllProducts["msg"]).length > 0) {
+          this.chkAllProducts["msg"].forEach(
+            (value, key) => {
+              var prepareProductData = {};
+              prepareProductData["product_id"] = value.product_id;
+              prepareProductData["ec_deliverytime"] = dt;
+              this.updatedBolProducts[value.product_id] = prepareProductData;
+            }
+          )
+        }
+      }
+    }
+
+
+    var filterBolProcessData = this.updatedBolProducts.filter(function () { return true; });
+    if (filterBolProcessData.length > 0) {
+      this.http.post(environment.webservicebaseUrl + "/save-bol-delivery-time", filterBolProcessData).subscribe(responseData => {
+        if (responseData["msg"] == "done") {
+          if (this.isChkAllChecked == 0) {
+            this.updatedBolProducts = [];
+          } else if (this.isChkAllChecked == 1) {
+            this.chkAllCount = "(" + (this.chkAllProducts["msg"]).length + " Delivery Time Updated Successfully)";
+            this.updatedBolProducts = [];
+          }
+          this.loadAGGrid();
+        }
+      });
+    }
+  }
+
+  getAllProducts(event) {
+    if (event.target.checked) {
+      var currentsql = localStorage.getItem("currentSql")?.trim();
+      this.chkAllCount = "(Please Wait ...)";
+      this.http.post(environment.webservicebaseUrl + "/all-products", currentsql).subscribe(responseData => {
+        this.chkAllProducts = responseData;
+        this.chkAllCount = "(" + responseData["msg"].length + ")";
+        this.isChkAllChecked = 1;
+
+        this.api.forEachNode((rowNode) => {
+          rowNode.setSelected(true)
+        });
+
+
+      });
+    } else {
+      this.chkAllCount = "";
+      this.chkAllProducts = "";
+      this.isChkAllChecked = 0;
+
+      this.api.forEachNode((rowNode) => {
+        rowNode.setSelected(false)
+      });
+
+    }
+  }
+
 }
 
 function createServerSideDatasource(server: any): IServerSideDatasource {
@@ -83,6 +186,7 @@ function createServerSideDatasource(server: any): IServerSideDatasource {
           }
           //params.successCallback(response.rows, response.lastRow);
           var limitIndex = (response.currentSql).indexOf("limit");
+          localStorage.setItem("currentSql", (response.currentSql).substring(0, limitIndex));
           params.success({ rowData: response.rows, rowCount: response.lastRow })
 
         })
